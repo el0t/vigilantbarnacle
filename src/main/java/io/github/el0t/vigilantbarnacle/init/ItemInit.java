75994e3f8a389041f96985569c50661f5d8f6ae5
el0t <elliottgold96@gmail.com>
2023-04-27 19:43:02 -0400

    added 5 swords

--- src/main/java/io/github/el0t/vigilantbarnacle/init/ItemInit.java
+++ src/main/java/io/github/el0t/vigilantbarnacle/init/ItemInit.java
@@ -1,9 +1,8 @@
 package io.github.el0t.vigilantbarnacle.init;
 
 import io.github.el0t.vigilantbarnacle.VigilantBarnacle;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
+import io.github.el0t.vigilantbarnacle.util.tiers.VBTiers;
+import net.minecraft.world.item.*;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -13,4 +12,12 @@ public class ItemInit {
 
     public static final RegistryObject<Item> ORICHALCUM = ITEMS.register("orichalcum", () -> new Item(new Item.Properties().rarity(Rarity.RARE)));
     public static final RegistryObject<Item> YGGDRASIL_LEAF = ITEMS.register("yggdrasil_leaf", () -> new Item(new Item.Properties().rarity(Rarity.EPIC)));
+
+    public static final RegistryObject<SwordItem> CYPRESS_STICK = ITEMS.register("cypress_stick",
+            () -> new SwordItem(Tiers.WOOD, 1, 3.5F, new Item.Properties()));  //tier, attack_damage, attack_speed, props
+    public static final RegistryObject<SwordItem> SOLDIERS_SWORD = ITEMS.register("soldiers_sword", () -> new SwordItem(Tiers.IRON, 3, -2.4F, new Item.Properties()));
+    public static final RegistryObject<SwordItem> RAPIER = ITEMS.register("rapier", () -> new SwordItem(Tiers.IRON, 3, 2.0F, new Item.Properties()));
+    public static final RegistryObject<SwordItem> STEEL_BROADSWORD = ITEMS.register("steel_broadsword", () -> new SwordItem(Tiers.IRON, 4, -2.8F, new Item.Properties()));
+    public static final RegistryObject<SwordItem> TEMPLARS_SWORD = ITEMS.register("templars_sword", ()-> new SwordItem(Tiers.IRON, 3, -2.4F, new Item.Properties()));
+
 }
